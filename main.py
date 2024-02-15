088f2973f2d04cf3dd44afbf4815c21cb3e6a635
Ali Zahedi Sohrofrouzani <75866665+alizahedihere@users.noreply.github.com>
2024-02-15 13:31:58 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from  PIL import Image
 
-imam = Image.open("test.jpg")
+orange = Image.open("orange.jpg")
 moon = Image.open("moon.jpg")
 
 box=(240,79,465,360)
-imam = imam.crop(box)
+orange = orange.crop(box)
 
-imam = imam.resize((111, 106), Image.BICUBIC)
+orange = orange.resize((111, 106), Image.BICUBIC)
 
-imam_red, imam_green, imam_blue = imam.split()
-list_imam_red = list(imam_red.getdata())
-list_imam_green = list(imam_green.getdata())
-list_imam_blue = list(imam_blue.getdata())
+orange_red, orange_green, orange_blue = orange.split()
+list_orange_red = list(orange_red.getdata())
+list_orange_green = list(orange_green.getdata())
+list_orange_blue = list(orange_blue.getdata())
 
-list_imam_red = list(map(lambda x: x - 150, list_imam_red))
-list_imam_green = list(map(lambda x: x - 150, list_imam_green))
-list_imam_blue = list(map(lambda x: x - 150, list_imam_blue))
+list_orange_red = list(map(lambda x: x - 150, list_orange_red))
+list_orange_green = list(map(lambda x: x - 150, list_orange_green))
+list_orange_blue = list(map(lambda x: x - 150, list_orange_blue))
 
-imam_red.putdata(list_imam_red)
-imam_green.putdata(list_imam_green)
-imam_blue.putdata(list_imam_blue)
+orange_red.putdata(list_orange_red)
+orange_green.putdata(list_orange_green)
+orange_blue.putdata(list_orange_blue)
 
-new_imam = Image.merge("RGB",(imam_red,imam_green,imam_blue))
+new_orange = Image.merge("RGB",(orange_red,orange_green,orange_blue))
 
 moon_crop = moon.crop((173, 59, 173 + 111, 59 + 106))
 
-imam_red, imam_green, imam_blue = new_imam.split()
+orange_red, orange_green, orange_blue = new_orange.split()
 moon_red, moon_green, moon_blue = moon_crop.split()
 
-list_imam_red = list(imam_red.getdata())
-list_imam_green = list(imam_green.getdata())
-list_imam_blue = list(imam_blue.getdata())
+list_orange_red = list(orange_red.getdata())
+list_orange_green = list(orange_green.getdata())
+list_orange_blue = list(orange_blue.getdata())
 list_moon_red = list(moon_red.getdata())
 list_moon_green = list(moon_green.getdata())
 list_moon_blue = list(moon_blue.getdata())
 
-list_avg_red = [(a + b) // 2 for a, b in zip(list_imam_red, list_moon_red)]
-list_avg_green = [(a + b) // 2 for a, b in zip(list_imam_green, list_moon_green)]
-list_avg_blue = [(a + b) // 2 for a, b in zip(list_imam_blue, list_moon_blue)]
+list_avg_red = [(a + b) // 2 for a, b in zip(list_orange_red, list_moon_red)]
+list_avg_green = [(a + b) // 2 for a, b in zip(list_orange_green, list_moon_green)]
+list_avg_blue = [(a + b) // 2 for a, b in zip(list_orange_blue, list_moon_blue)]
 
 avg_red = Image.new("L", (111, 106))
 avg_green = Image.new("L", (111, 106))
@@ -51,4 +51,4 @@ avg_image = Image.merge("RGB", (avg_red, avg_green, avg_blue))
 
 moon.paste(avg_image, (173, 59))
 moon.show()
-moon.save("imam_on_moon.jpg")
+moon.save("test_on_moon.jpg")
